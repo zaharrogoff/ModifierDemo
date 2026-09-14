903b6eb20c0392b199376cdfb5817d48ad99b4fb
zahar <zaharrogoff@gmail.com>
2026-09-14 14:59:51 +0700

    Add DemoScreen

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
 
 
@@ -36,4 +40,14 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+
+    Text(
+        "Hello Compose",
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+
 }
