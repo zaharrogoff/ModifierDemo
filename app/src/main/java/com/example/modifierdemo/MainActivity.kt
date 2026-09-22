9d63ac5249eb0a45b942a35ccc9ac5b05be0c71f
zahar <zaharrogoff@gmail.com>
2026-09-22 17:14:48 +0700

    Change modifier order: padding first, then border

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -47,8 +47,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
     val mymodifier = modifier
-        .border(width = 2.dp, color = Color.Black)
         .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Black)
 
     Text(
         "Hello Compose",
